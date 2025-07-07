d44308eb0c0aaccd778da95f2d188615dd0adbe7
Pratik Patel <pratik.patel@cloudbclabs.com>
2025-07-07 22:17:27 +0530

    Configuration Updated
    Pm2 configurations
    env file configurations

--- server.js
+++ server.js
@@ -5,9 +5,22 @@ const PORT = process.env.PORT || 5000;
 
 const mainRoutes = require("./routes/masterDataRoutes");
 
+const path = require("path");
+const dotenv = require("dotenv");
+//const envFile = process.env.NODE_ENV === "production" ? ".env.production" : ".env";
+
+const envPath = process.env.NODE_ENV === "production" 
+  ? path.resolve(__dirname, ".env.production") 
+  : path.resolve(__dirname, ".env");
+
+console.log(`✅ Loading environment file: ${envPath}`);
+console.log(`📝 DB Host: ${process.env.DB_HOST || 'development'}`); 
+dotenv.config({ path: envPath });
+
+// dotenv.config({ path: path.resolve(__dirname, envFile) });
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173'
+  origin: '*'
 }));
 app.use(express.json());
 
@@ -22,4 +35,6 @@ app.get("/", (req, res) => {
 // Start server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  console.log(`📝 Environment: ${process.env.NODE_ENV || 'development'}`);
+  console.log(`📝 DB Host: ${process.env.DB_HOST || 'development'}`);  
 });
